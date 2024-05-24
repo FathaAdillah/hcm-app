4fc6a168863bb583acf34f20e04ecac879f50a73
Fatha Adillah <fatha.adillah12@gmail.com>
2024-05-24 19:54:00 +0700

    Create Reset Password

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="https://nauval.in/">Fataroni Adillah Rachman</a>
+        Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="https://fatharoni-resume.domcloud.dev/">Fatharoni Adillah Rachman</a>
     </div>
     <div class="footer-right">
         2.3.0
