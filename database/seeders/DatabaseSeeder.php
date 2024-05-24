8f4f3f633842dac2628f82ef5a989bed155dee3b
Fatha Adillah <fatha.adillah12@gmail.com>
2024-05-24 16:20:08 +0700

    Commit db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Fatha',
+            'email' => 'aa@bb.cc',
         ]);
     }
 }
